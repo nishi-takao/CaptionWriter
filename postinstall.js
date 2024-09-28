3cfe9a055fa4b119ff860a8fc808103cce8f1db6
NISHI, Takao <nishi-takao@sangaku.es.osaka-u.jp>
2024-09-28 18:38:12 +0900

    fixed some minor isuues

--- postinstall.js
+++ postinstall.js
@@ -4,22 +4,26 @@ const Path=require('path');
 
 const Files={
     'node_modules/roboto-fontface/fonts/roboto/Roboto-Regular.woff2':
-    './fonts/',
+    'fonts',
     'node_modules/roboto-fontface/fonts/roboto/Roboto-Bold.woff2':
-    './fonts/',
+    'fonts',
     'node_modules/@fortawesome/fontawesome-free/webfonts/fa-regular-400.ttf':
-    './fonts/',
+    'fonts',
     'node_modules/@fortawesome/fontawesome-free/webfonts/fa-regular-400.woff2':
-    './fonts/',
+    'fonts',
     'node_modules/@fortawesome/fontawesome-free/webfonts/fa-solid-900.ttf':
-    './fonts/',
+    'fonts',
     'node_modules/@fortawesome/fontawesome-free/webfonts/fa-solid-900.woff2':
-    './fonts/',
-    'node_modules/@fortawesome/fontawesome-free/css/fontawesome.min.css':'./css/'
+    'fonts',
+    'node_modules/@fortawesome/fontawesome-free/css/fontawesome.min.css':
+    'css'
 };
 
 Object.entries(Files).forEach(
     ([src,dstdir])=>{
-	FS.cpSync(src,Path.join(dstdir,Path.basename(src)));
+	FS.cpSync(
+	    Path.join.apply(null,src.split('/')),
+	    Path.join(dstdir,Path.basename(src))
+	);
     }
 );
