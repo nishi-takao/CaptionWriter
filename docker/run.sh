4f564c708c5765743c193b39e3ce95aedd3312d9
NISHI, Takao <nishi-takao@sangaku.es.osaka-u.jp>
2024-11-19 13:33:32 +0900

    fixed docker/run.sh CONF_DIR issue

--- docker/run.sh
+++ docker/run.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/bash
 set -e
 if [ -z "$1" ]; then \
-   echo "$0 SOME/WHERE/IMAGES" >& 2
+   echo "$0 /SOME/WHERE/IMAGES" >& 2
    exit -1;
 fi
 
+function expand_path(){
+   (cd -- "$(dirname -- "$1")" && pwd)
+}
+
 EXECUID=$(id -u $(who am i|cut -d ' ' -f 1))
-CONF_DIR="$(dirname $(pwd))/config"
-DIRNAME="$(cd -- "$(dirname -- "$1")" && pwd)"
-DATA_DIR="${DIRNAME%/}/$(basename -- "$1")"
+CONF_DIR="$(expand_path "$0")/config"
+DATA_DIR="$(expand_path "$1")/$(basename -- "$1")"
 
 exec docker run -it --rm \
        -e EXECUID=$EXECUID \
@@ -17,8 +20,8 @@ exec docker run -it --rm \
        -e DISPLAY=$DISPLAY \
        -v /tmp/.X11-unix:/tmp/.X11-unix \
        -v "$HOME/.Xauthority:/home/node/.Xauthority:rw" \
-       -v $CONF_DIR:/home/node/app/config \
-       -v $DATA_DIR:/mnt/data \
+       -v "$CONF_DIR:/home/node/app/config" \
+       -v "$DATA_DIR:/mnt/data" \
        --network=host \
        --device /dev/dri \
        --cap-add=SYS_ADMIN \
